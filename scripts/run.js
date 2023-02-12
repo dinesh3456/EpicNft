ce832261d9b3b6b57c8caef2efacb3853b9ad57c
Dinesh <dinesh2952002@outlook.com>
2023-02-13 04:52:51 +0530

    Initial commit

---
+++ scripts/run.js
@@ -0,0 +1,27 @@
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory("EpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to: ", nftContract.address);
+
+  let txn = await nftContract.makeAnEpicNFT();
+  //await txn.wait();
+  let NFTcount = await nftContract.getTotalNFTMinted();
+  //await NFTcount.wait();
+
+  txn = await nftContract.makeAnEpicNFT();
+  //await txn.wait();
+  NFTcount = await nftContract.getTotalNFTMinted();
+  //await NFTcount.wait();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
+};
+runMain();
